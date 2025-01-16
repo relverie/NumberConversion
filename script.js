f1047785439fe0c3550d59e3e867794854a266d8
relverie <sillyrelverie@gmail.com>
2025-01-16 15:06:15 +0000

    updated :3
    parseint function - grab the element from the textbox
    
    finished denary2binary
    finished addingdenary
    finished hex2binary

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ if number type is correct:
 else:
 return error
 */
-
+//get the input!!!!!
+function parseInt(str){
+    let num;
+    num = document.getElementById("input");
+    return num;
+}
 //verify denary (must be int, must be in range 0-255)
 function verifyDenary(){
 
@@ -30,12 +35,53 @@ function verifyHexadecimal(){
 function verifyBinary(){
 
 }
-//convert denary into binary
+//convert denary into binary :3
+function Den2binary(input){
+    let number = parseInt(input);
+    let binaryResult =  []
+    for (let i = 0; i < 9; i++) {
+        binaryResult.push(binaryResult[i] = number % 2)
+        number = number / 2
+    }
+    let output = binaryResult.join();
+}
 //convert denary into hexadecimal
-//add two denary numbers
+function den2Hexadecimal(input)
 
+}
+//add two denary numbers :3
+function add(input1, input2){
+    return input1 + input2;
+}
 //convert binary into denary
-//convert hexadecimal into denary
+function Bin2denary(input){
+    let splitbinary = input.split("")
+    let map = new Map();
+
+}
+//convert hexadecimal into binary
+function Hex2binary(input){
+    let map = new Map(); //silly
+    map.set("0", "0000");
+    map.set("1", "0001");
+    map.set("2", "0010");
+    map.set("3", "0011");
+    map.set("4", "0100");
+    map.set("5", "0101");
+    map.set("6", "0110");
+    map.set("7", "0111");
+    map.set("8", "1000");
+    map.set("9", "1001");
+    map.set("A", "1010");
+    map.set("B", "1011");
+    map.set("C", "1100");
+    map.set("D", "1101");
+    map.set("E", "1110");
+    map.set("F", "1111");
+    let pt1 = map.get(input.slice(0,1));
+    let pt2 = map.get(input.slice(2,2));
+    return pt1.toString() + p2.toString();
+}
 
 //These three can be made by utilizing previous code
 //convert binary into hexadecimal
